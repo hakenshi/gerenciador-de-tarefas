a05d8e079f8e9d36ff4294c83b7de16d00c88406
hakenshi <fkdias025@gmail.com>
2025-02-11 22:20:20 -0300

    init

---
+++ src/main/java/com/mycompany/gerenciadordetarefas/GerenciadorDeTarefas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gerenciadordetarefas;
+
+/**
+ *
+ * @author lonely
+ */
+public class GerenciadorDeTarefas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
